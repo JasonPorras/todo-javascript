84f00f8e1c80142026dcbd1fecd408298b07025d
Víctor Peña <92888026+Wiktor124@users.noreply.github.com>
2023-03-27 12:16:05 -0600

    finished deleted all completed method

--- main.js
+++ main.js
@@ -1,99 +1,120 @@
 class TodoList {
+  static deleteAllCompleted = document.querySelector('#deleteAllCompleted')
+
   constructor(input, addBtn, ul, empty) {
-    this.input = input;
-    this.addBtn = addBtn;
-    this.ul = ul;
-    this.empty = empty;
+    this.input = input
+    this.addBtn = addBtn
+    this.ul = ul
+    this.empty = empty
 
-    this.addBtn.addEventListener("click", (e) => {
-      e.preventDefault();
-      this.addItem();
+    this.addBtn.addEventListener('click', (e) => {
+      e.preventDefault()
+      this.addItem()
+    })
 
-    });
-    TodoList.complete();
+    TodoList.completed()
+    TodoList.deleteCompleted()
   }
 
   addItem() {
-    const text = this.input.value;
-    const checkbtn = document.createElement("button")
-    checkbtn.className = 'completedBtn';
-    checkbtn.textContent = '+';
+    const text = this.input.value
+    const checkbtn = document.createElement('button')
+    checkbtn.className = 'completedBtn'
+    checkbtn.textContent = '+'
 
-    const category = document.createElement("option");
+    const category = document.createElement('option')
 
-    if (text !== "" && option.value !== "category") {
-      const li = document.createElement("li");
-      const p = document.createElement("p");
-      p.className = "parrafo";
-      p.textContent = text;
+    if (text !== '' && option.value !== 'category') {
+      const li = document.createElement('li')
+      const p = document.createElement('p')
+      p.className = 'parrafo'
+      p.textContent = text
 
-      li.appendChild(p);
-      this.ul.appendChild(li);
-      li.appendChild(category);
-      li.appendChild(checkbtn);
-      li.appendChild(this.addDeleteBtn());
+      li.appendChild(p)
+      this.ul.appendChild(li)
+      li.appendChild(category)
+      li.appendChild(checkbtn)
+      li.appendChild(this.addDeleteBtn())
 
-      this.input.value = "";
-      this.empty.style.display = "none";
+      this.input.value = ''
+      this.empty.style.display = 'none'
 
-      category.innerHTML = option.value;
-      category.className = "filter-todo";
+      category.innerHTML = option.value
+      category.className = 'filter-todo'
 
-      const items = document.querySelectorAll("li");
+      const items = document.querySelectorAll('li')
 
-       if (items.length === 0) {
-       this.option.value = "";
+      if (items.length === 0) {
+        this.option.value = ''
       }
+    } else {
+      alert("There's missing Category or add a Task!")
     }
-
   }
 
-  static complete() {
+  static completed() {
     document.querySelector('.todos').addEventListener('click', (e) => {
-
       if (e.target.matches('.completedBtn')) {
+        const parentLi = e.target.parentNode
+        parentLi.querySelector('p').classList.toggle('completedTask')
+        parentLi.querySelector('option').classList.toggle('completedTask')
 
-        const parentLi = e.target.parentNode;
-        parentLi.querySelector('p').classList.toggle('completedTask');
-        parentLi.querySelector('option').classList.toggle('completedTask');
+        // add data-task as completed if is it
+        parentLi.hasAttribute('data-task')
+        ? parentLi.removeAttribute('data-task')
+        : parentLi.setAttribute('data-task', 'completed')
 
+        // anable or disable delete all completed button
+        document.querySelectorAll('[data-task="completed"]').length > 0
+        ? TodoList.deleteAllCompleted.disabled = false
+        : TodoList.deleteAllCompleted.disabled = true
       }
     })
   }
 
+  // delete all tasks completed
+  static deleteCompleted() {
+    TodoList.deleteAllCompleted.disabled = true
+
+    TodoList.deleteAllCompleted.addEventListener('click', () => {
+      const completedTasks = document.querySelectorAll('[data-task="completed"]')
+
+      for (let i = 0; i < completedTasks.length; i++) {
+        completedTasks[i].remove()
+      }
+    })
+  }
 
   addDeleteBtn() {
-    const btnCompleted = document.createElement("button");
-    const deleteBtn = document.createElement("button");
+    const btnCompleted = document.createElement('button')
+    const deleteBtn = document.createElement('button')
 
+    btnCompleted.textContent = '*'
+    btnCompleted.className = 'btn-check '
 
-    btnCompleted.textContent = "*";
-    btnCompleted.className = "btn-check ";
+    deleteBtn.textContent = 'X'
+    deleteBtn.className = 'btn-delete'
 
-    deleteBtn.textContent = "X";
-    deleteBtn.className = "btn-delete";
+    deleteBtn.addEventListener('click', () => {
+      const item = deleteBtn.parentElement
+      item.parentNode.removeChild(item)
 
-    deleteBtn.addEventListener("click", () => {
-      const item = deleteBtn.parentElement;
-      item.parentNode.removeChild(item);
-
-      const items = document.querySelectorAll("li");
+      const items = document.querySelectorAll('li')
 
       if (items.length === 0) {
-        this.complete = "";
-        this.empty.style.display = "block";
+        this.complete = ''
+        this.empty.style.display = 'block'
       }
-    });
+    })
 
-    return deleteBtn;
+    return deleteBtn
   }
 }
 
+const input = document.querySelector('input')
+const addBtn = document.querySelector('#btn-add')
+const option = document.querySelector('.filter-todo')
+const ul = document.querySelector('ul')
+const empty = document.querySelector('.empty')
 
-const input = document.querySelector("input");
-const addBtn = document.querySelector("#btn-add");
-const option = document.querySelector(".filter-todo");
-const ul = document.querySelector("ul");
-const empty = document.querySelector(".empty");
-
-const todoList = new TodoList(input, addBtn, ul, empty);
+const todoList = new TodoList(input, addBtn, ul, empty)
